be69bf18fad19541d5eece6654a070d1d961502a
Kleberson <61327766+Klevilela@users.noreply.github.com>
2022-05-09 22:12:41 -0300

    Add files via upload

--- algoritmo-ADS-IFPI_2022.1/prova1_algoritmos_ads-2022.1 -  kleberson/coracao.py
+++ algoritmo-ADS-IFPI_2022.1/prova1_algoritmos_ads-2022.1 -  kleberson/coracao.py
@@ -3,11 +3,22 @@ fc_atual = int(input('Informe a sua frequência cardíaca atual: '))
 
 fc_max = 220 - idade
 
-intensidade_moderada = fc_max * 0.5
-intensidade_controle_do_peso = 0
-intensidade_aerobica = 0
-intensidade_anaerobica = 0
-intensidade_esforco_max = 0
+intensidade_moderada1 = fc_max * (50 / 100)
+intensidade_moderada2 = fc_max * (60 / 100)
+
+intensidade_controle_de_peso1 = fc_max * (60 /100)
+intensidade_controle_de_peso2 = fc_max * (70 /100)
+
+
+intensidade_aerobica1 = fc_max * (70 / 100)
+intensidade_aerobica2 = fc_max * (80 / 100)
+
+intensidade_anaerobica1 = fc_max * (80 / 100)
+intensidade_anaerobica2 = fc_max * (90 / 100)
+
+
+intensidade_esforco_max1 = fc_max * (90/100)
+intensidade_esforco_max2 = fc_max 
 
 atividade_moderada = 60
 controle_de_peso = 70
@@ -15,18 +26,20 @@ aerobica = 80
 anaerobica = 90
 esforco_maximo = 100
 
-if fc_atual >= intensidade_moderada and fc_atual <= atividade_moderada:
-    print('Zona da Frequência Cardíaca atual {} - moderada\nLimites da FC de acordo com a idade informada:  {} - {}'.format(fc_atual, 50, atividade_moderada))
+if fc_atual <= intensidade_moderada1 and fc_atual <= intensidade_moderada2:
+    print('Zona da Frequência Cardíaca atual {} - moderada\nLimites da FC de acordo com a idade informada:  {}(%) - {}(%) da frequência máxima atual'.format(fc_atual, 50, atividade_moderada))
 
-if fc_atual >= 60 and fc_atual <= controle_de_peso:
-    print('Zona da Frequência Cardíaca atual {}\nLimites da FC de acordo com a idade informada:  {} - {}'.format(fc_atual, 60, atividade_moderada)) 
+if fc_atual >= intensidade_controle_de_peso1 and fc_atual <= intensidade_controle_de_peso2:
+    print('Zona da Frequência Cardíaca atual {} - controle de peso\nLimites da FC de acordo com a idade informada:  {}(%) - {}(%) da frequência máxima atual'.format(fc_atual, 60, controle_de_peso)) 
 
-if fc_atual >= controle_de_peso and fc_atual <= aerobica:
-    print('Zona da Frequência Cardíaca atual {}\nLimites da FC de acordo com a idade informada:  {} - {}'.format(fc_atual, 70, atividade_moderada)) 
+if fc_atual >= intensidade_aerobica1 and fc_atual <= intensidade_aerobica2:
+    print('Zona da Frequência Cardíaca atual {} - aeróbica\nLimites da FC de acordo com a idade informada:  {}(%) - {}(%) da frequência máxima atual'.format(fc_atual, 70, aerobica)) 
 
-if fc_atual <= atividade_moderada:
-    print('Zona da Frequência Cardíaca atual {}\nLimites da FC de acordo com a idade informada:  {} - {}'.format(fc_atual, 80, atividade_moderada))
+if fc_atual >= intensidade_anaerobica1 and fc_atual <= intensidade_anaerobica2:
+    print('Zona da Frequência Cardíaca atual {} - anaeróbica\nLimites da FC de acordo com a idade informada:  {}(%) - {}(%) da frequência máxima atual'.format(fc_atual, 80, anaerobica))
 
-if fc_atual <= atividade_moderada:
-    print('Zona da Frequência Cardíaca atual {}\nLimites da FC de acordo com a idade informada:  {} - {}'.format(fc_atual, 90, atividade_moderada))
-      
+if fc_atual >= intensidade_esforco_max1 and fc_atual >= intensidade_esforco_max2:
+    print('Zona da Frequência Cardíaca atual {} - esforço máximo\nLimites da FC de acordo com a idade informada:  {}(%) - {}(%) da frequência máxima atual'.format(fc_atual, 90, esforco_maximo))
+
+print(intensidade_moderada1)
+print(intensidade_moderada2)      
